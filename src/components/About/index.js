37064ae2ad69d4f0b479248185e2572c27d09c52
Narek Germirlian <ngermirlian@gmail.com>
2025-09-01 01:50:47 -0700

    Added link to About

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,38 @@
 import React from "react";
 
 import "./About.css";
-import aboutImage from "../../images/common/ABOUT.png";
+import aboutImageNOBOX from "../../images/common/ABOUT_NOBOX.png";
 
 const About = () => {
   return (
     <div className="about-container">
       <div className="about-content">
-        <img src={aboutImage} alt="About" className="about-image" />
+        <div className="about-section">
+          <img
+            src={aboutImageNOBOX}
+            alt="About Daily Bruin"
+            className="about-img"
+          />
+
+          <div className="dashed-lines1"></div>
+          <div className="dashed-lines2"></div>
+
+          <div className="apply-overlay">
+            <p>
+              Like what you see? <br />
+              <a
+                href="https://apply.uclastudentmedia.com/"
+                className="apply-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                APPLY HERE
+              </a>
+              <br />
+              for a Daily Bruin internship
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
