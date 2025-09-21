e35b30384f64a078685a92dc3e6a50ed721ec2ed
ycho1908 <yehyeoncho@gmail.com>
2025-09-20 22:14:49 -0700

    fixed articlecards for aml pulling

--- src/components/ArticleCard/Horz.js
+++ src/components/ArticleCard/Horz.js
@@ -1,21 +1,24 @@
 import React from "react";
 import "./Horz.css";
 import paperclip from "../../images/common/paper-clip.png";
-import sampleImg from "../../images/common/sample-img.png";
+// import sampleImg from "../../images/common/sample-img.png";
 
-const Horz = () => (
-  <a className="article-card-horz" href="https://unsplash.com/s/photos/cat">
+const Horz = ({
+  image,
+  article_url,
+  article_title,
+  author_byline,
+  article_text
+}) => (
+  <a className="article-card-horz" href={article_url}>
     <img src={paperclip} alt="paperclip" className="paperclip-horz" />
     <div className="card-content-horz">
       <div className="text-col">
-        <h2 className="headline-horz">
-          Headline Goes Here! <br />
-          Here are some more words until we reach the 82 character limit
-        </h2>
-        <p className="byline-horz">By this person, more bylines and maybe even more names</p>
+        <h2 className="headline-horz">{article_title}</h2>
+        <p className="byline-horz">By {author_byline}</p>
       </div>
       <div className="img-col">
-        <img src={sampleImg} alt="article" className="article-img-horz" />
+        <img src={image} alt="article" className="article-img-horz" />
       </div>
     </div>
   </a>
--- src/components/ArticleCard/Vert.js
+++ src/components/ArticleCard/Vert.js
@@ -1,24 +1,27 @@
 import React from "react";
 import "./ArticleCard.css";
 import paperclip from "../../images/common/paper-clip.png"; 
-import sampleImg from "../../images/common/sample-img.png";
+// import sampleImg from "../../images/common/sample-img.png";
 
-const Vert = () => {
-  return (
-    <a className="article-card" href="https://unsplash.com/s/photos/cat">
-      <img src={paperclip} alt="paperclip" className="paperclip" />
-      <div className="card-content">
-        <h2 className="headline">
-          Headline Goes Here! <br />
-          Here are some more words until we reach the 82 character limit
-        </h2>
-        <img src={sampleImg} alt="article" className="article-img" />
-        <p className="byline">
-          By this person, more bylines and maybe even more names AHHHHHHHH
-        </p>
+const Vert = ({
+  image,
+  article_url,
+  article_title,
+  author_byline,
+  article_text
+}) => (
+  <a className="article-card-horz" href={article_url}>
+    <img src={paperclip} alt="paperclip" className="paperclip-horz" />
+    <div className="card-content-horz">
+      <div className="text-col">
+        <h2 className="headline-horz">{article_title}</h2>
+        <p className="byline-horz">By {author_byline}</p>
       </div>
-    </a>
-  );
-};
+      <div className="img-col">
+        <img src={image} alt="article" className="article-img-horz" />
+      </div>
+    </div>
+  </a>
+);
 
 export default Vert;
